15235a00d20ef140383afac9b6b68cd667bcb436
Christian Panagiotopoulos <christian.panagiotopoulos@web.de>
2017-08-09 14:29:59 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+bookproject
+===========
+
+A Symfony project created on August 9, 2017, 12:33 pm.
---
+++ src/AppBundle/Entity/Book.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace AppBundle\Entity;
+
+/**
+ * Description of Book
+ *
+ * @author chris_000
+ */
+class Book {
+    //put your code here
+}
